14b31fc7131cd03ae2a287056da1476cacb02515
Rita <margaritam2706@gmail.com>
2014-10-22 16:58:09 +0400

    add same methods

--- src/main/java/ru/compscicenter/java2014/collections/MultiSetImpl.java
+++ src/main/java/ru/compscicenter/java2014/collections/MultiSetImpl.java
@@ -1,7 +1,5 @@
 package ru.compscicenter.java2014.collections;
 
-import ru.compscicenter.java2014.collections.MultiSet;
-
 import java.util.*;
 import java.util.function.Consumer;
 import java.util.function.Predicate;
@@ -10,18 +8,31 @@ import java.util.stream.Stream;
 /**
  * Created by Markina Margarita on 20.10.14.
  */
+
 public class MultiSetImpl<E> implements MultiSet<E> {
-  Map<E, Integer> MultiSet = new HashMap<>();
-  int size = 0;
+  private Map<E, Integer> map;
+  private int size;
+
+
+  public MultiSetImpl() {
+    map = new HashMap<>();
+    size = 0;
+  }
+
+  public MultiSetImpl(Collection<? extends E> c) {
+    this();
+    addAll(c);
+  }
+
 
   /**
    * Creates a {@link java.util.Spliterator} over the elements in this collection.
-   * <p/>
+   * <p>
    * Implementations should document characteristic values reported by the
    * spliterator.  Such characteristic values are not required to be reported
    * if the spliterator reports {@link java.util.Spliterator#SIZED} and this collection
    * contains no elements.
-   * <p/>
+   * <p>
    * <p>The default implementation should be overridden by subclasses that
    * can return a more efficient spliterator.  In order to
    * preserve expected laziness behavior for the {@link #stream()} and
@@ -46,11 +57,11 @@ public class MultiSetImpl<E> implements MultiSet<E> {
    * <em><a href="Spliterator.html#binding">late-binding</a></em> spliterator
    * from the collections's {@code Iterator}.  The spliterator inherits the
    * <em>fail-fast</em> properties of the collection's iterator.
-   * <p/>
+   * <p>
    * The created {@code Spliterator} reports {@link java.util.Spliterator#SIZED}.
    * @implNote The created {@code Spliterator} additionally reports
    * {@link java.util.Spliterator#SUBSIZED}.
-   * <p/>
+   * <p>
    * <p>If a spliterator covers no elements then the reporting of additional
    * characteristic values, beyond that of {@code SIZED} and {@code SUBSIZED},
    * does not aid clients to control, specialize or simplify computation.
@@ -67,7 +78,7 @@ public class MultiSetImpl<E> implements MultiSet<E> {
 
   /**
    * Returns a sequential {@code Stream} with this collection as its source.
-   * <p/>
+   * <p>
    * <p>This method should be overridden when the {@link #spliterator()}
    * method cannot return a spliterator that is {@code IMMUTABLE},
    * {@code CONCURRENT}, or <em>late-binding</em>. (See {@link #spliterator()}
@@ -86,7 +97,7 @@ public class MultiSetImpl<E> implements MultiSet<E> {
   /**
    * Returns a possibly parallel {@code Stream} with this collection as its
    * source.  It is allowable for this method to return a sequential stream.
-   * <p/>
+   * <p>
    * <p>This method should be overridden when the {@link #spliterator()}
    * method cannot return a spliterator that is {@code IMMUTABLE},
    * {@code CONCURRENT}, or <em>late-binding</em>. (See {@link #spliterator()}
@@ -168,85 +179,79 @@ public class MultiSetImpl<E> implements MultiSet<E> {
 
   }
 
-  /**
-   * Adds a single occurrence of the specified element to this multiset.
-   * <p/>
-   * Always returns <code>true</code>, because multiset always allows adding
-   * both new elements and new occurrences of known elements.
-   *
-   * @param e element to add
-   * @return <code>true</code>
-   */
   @Override
   public boolean add(E e) {
-    return false;
+    add(e, 1);
+    return true;
   }
 
-  /**
-   * Adds multiple occurrences of the specified element to this multiset.
-   *
-   * @param e           element to add
-   * @param occurrences number of element occurrences to add; can't be negative
-   * @return the count of element occurrences before the operation; possibly zero
-   * @throws IllegalArgumentException if <code>occurrences</code> is negative
-   */
   @Override
-  public int add(E e, int occurrences) {
-    return 0;
+  public int add(E e, int occurrences) throws IllegalArgumentException {
+    if (occurrences < 0) {
+      throw new IllegalArgumentException();
+    }
+    int occurrencesBeforeTheOperation = 0;
+    size += occurrences;
+    if (map.containsKey(e)) {
+      occurrencesBeforeTheOperation = map.get(e);
+      map.put(e, occurrencesBeforeTheOperation + occurrences);
+    } else {
+      map.put(e, occurrences);
+    }
+    return occurrencesBeforeTheOperation;
   }
 
-  /**
-   * Removes a single occurrence of the specified element from this multiset, if present.
-   *
-   * @param e element to remove
-   * @return <code>true</code> if the element was found and removed
-   */
   @Override
   public boolean remove(Object e) {
-    return false;
+    if (map.containsKey(e)) {
+      size--;
+      if (map.get(e) == 0) {
+        map.remove(e);
+      } else {
+        map.put((E) e, map.get(e) - 1);
+      }
+      return true;
+    } else {
+      return false;
+    }
   }
 
-  /**
-   * Removes multiple occurrences of the specified element from this multiset, if present.
-   * If multiset contains fewer copies of the element than given by <code>occurrences</code>
-   * parameter, all occurrences are removed.
-   *
-   * @param e           element to remove
-   * @param occurrences number of element occurrences to remove; can't be negative
-   * @return the count of element occurrences before the operation; possibly zero
-   * @throws IllegalArgumentException if <code>occurrences</code> is negative
-   */
   @Override
   public int remove(Object e, int occurrences) {
-    return 0;
+    int occurrencesBefireTheOperation = 0;
+    if (occurrences < 0) {
+      throw new IllegalArgumentException();
+    }
+    if (map.containsKey(e)) {
+      occurrencesBefireTheOperation = map.get(e);
+      if (occurrencesBefireTheOperation <= occurrences) {
+        size -= occurrences;
+        map.remove(e);
+      } else {
+        size -= occurrences;
+        map.put((E) e, occurrencesBefireTheOperation - occurrences);
+      }
+    }
+    return occurrencesBefireTheOperation;
   }
 
-  /**
-   * Returns the number of occurrences of an element in this multiset,
-   * or <code>0</code> if multiset does not contain this element.
-   *
-   * @param e the element to whose occurrences are to be returned
-   * @return the number of occurrences of an element in this multiset
-   */
   @Override
   public int count(Object e) {
-    return 0;
+    if (map.containsKey(e)) {
+      return map.get(e);
+    } else {
+      return 0;
+    }
   }
 
-  /**
-   * Returns <code>true</code> if this multiset contains at least one occurrence of each element
-   * in the specified collection.
-   * <p/>
-   * This method ignores the occurrence count of an element in the two collections; it may still
-   * return <code>true</code> even if other collections contains several occurrences of an element
-   * and this multiset contains only one.
-   *
-   * @param c the collection of elements to be looked up in this multiset
-   * @return <code>true</code> if this multiset contains at least one occurrence of each element in <code>c</code>
-   */
   @Override
   public boolean containsAll(Collection<?> c) {
-    return false;
+    for (Object el : c) {
+      if(!map.containsKey(el)) {
+        return false;
+      }
+    }
+    return true;
   }
 
   /**
@@ -278,14 +283,18 @@ public class MultiSetImpl<E> implements MultiSet<E> {
     return false;
   }
 
-  /**
-   * Returns an array containing all of the elements in this multiset including all duplicates.
-   *
-   * @return an array containing all of the elements in this collection
-   */
   @Override
   public Object[] toArray() {
-    return new Object[0];
+    Object[] array = new Object[size];
+    int y = 0;
+    for(E el: map.keySet()) {
+      int cnt = map.get(el);
+      for(int i = 0; i < cnt; i++) {
+        array[y] = el;
+        y++;
+      }
+    }
+    return array;
   }
 
   /**
@@ -294,7 +303,7 @@ public class MultiSetImpl<E> implements MultiSet<E> {
    * If the collection fits in the specified array, it is returned therein.
    * Otherwise, a new array is allocated with the runtime type of the
    * specified array and the size of this collection.
-   * <p/>
+   * <p>
    * <p>If this collection fits in the specified array with room to spare
    * (i.e., the array has more elements than this collection), the element
    * in the array immediately following the end of the collection is set to
@@ -365,12 +374,8 @@ public class MultiSetImpl<E> implements MultiSet<E> {
     return false;
   }
 
-  /**
-   * Removes all of the elements from this multiset.
-   * The collection will be empty after this method returns.
-   */
   @Override
   public void clear() {
-
+    map.clear();
   }
 }
